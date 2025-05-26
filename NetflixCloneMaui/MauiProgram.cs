4c2e09fa2c03bf93505f0decfa7cfe63e34db720
opilane <opilane@HP-CZC0359NNZ>
2025-05-26 15:16:16 +0300

    part 3 done

--- NetflixCloneMaui/MauiProgram.cs
+++ NetflixCloneMaui/MauiProgram.cs
@@ -16,8 +16,8 @@ public static class MauiProgram
 			.UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
 			});
 
 #if DEBUG
