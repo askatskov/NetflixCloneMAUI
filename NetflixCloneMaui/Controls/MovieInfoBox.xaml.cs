2e44f99120af0a9fc6d8cf6bad4f7bfaa13cbbbd
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-02 23:34:08 +0300

    part 5

---
+++ NetflixCloneMaui/Controls/MovieInfoBox.xaml.cs
@@ -0,0 +1,20 @@
+using NetflixCloneMaui.Models;
+
+namespace Controls;
+
+public partial class MovieInfoBox : ContentView
+{
+	public static readonly BindableProperty MediaProperty =
+		BindableProperty.Create(nameof(Media), typeof(Media), typeof(MovieInfoBox), null);
+
+	public MovieInfoBox()
+	{
+		InitializeComponent();
+	}
+
+	public Media Media
+	{
+		get => (Media)GetValue(MovieInfoBox.MediaProperty);
+		set => SetValue(MovieInfoBox.MediaProperty, value);
+	}
+}
