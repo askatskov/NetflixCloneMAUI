5522c0aad8b7ea964d7527860ae65bc31c34f0ee
askatskov <160420092+askatskov@users.noreply.github.com>
2025-06-05 12:54:45 +0300

    part 6

--- NetflixCloneMaui/Controls/MovieInfoBox.xaml.cs
+++ NetflixCloneMaui/Controls/MovieInfoBox.xaml.cs
@@ -1,4 +1,5 @@
 using NetflixCloneMaui.Models;
+using System.Windows.Input;
 
 namespace Controls;
 
@@ -6,14 +7,30 @@ public partial class MovieInfoBox : ContentView
 {
 	public static readonly BindableProperty MediaProperty =
 		BindableProperty.Create(nameof(Media), typeof(Media), typeof(MovieInfoBox), null);
+
+	public event EventHandler Closed;
+
 	public MovieInfoBox()
 	{
 		InitializeComponent();
+		ClosedCommand = new Command(ExecuteClosedCommand);
 	}
 
 	public Media Media
 	{
-		get => (Media)GetValue(MovieInfoBox.MediaProperty);
-		set => SetValue(MovieInfoBox.MediaProperty, value);
+		get => (Media)GetValue(MediaProperty);
+		set => SetValue(MediaProperty, value);
+	}
+
+	public ICommand ClosedCommand { get; private set; }
+
+	private void ExecuteClosedCommand()
+	{
+		Closed?.Invoke(this, EventArgs.Empty);
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		Closed?.Invoke(this, EventArgs.Empty);
 	}
 }
--- NetflixCloneMaui/ViewModels/HomeViewModel.cs
+++ NetflixCloneMaui/ViewModels/HomeViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -11,59 +14,57 @@ using NetflixCloneMaui.Services;
 
 namespace NetflixCloneMaui.ViewModels
 {
-    public partial class HomeViewModel : ObservableObject
-    {
-        private readonly TmdbService _tmdbService;
+	public partial class HomeViewModel : ObservableObject
+	{
+		private readonly TmdbService _tmdbService;
 
-        public HomeViewModel(TmdbService tmdbService)
-        {
-            _tmdbService = tmdbService;
-        }
-        [ObservableProperty]
-        private Media _trendingMovie;
+		public HomeViewModel(TmdbService tmdbService)
+		{
+			_tmdbService = tmdbService;
+		}
+		[ObservableProperty]
+		private Media _trendingMovie;
 
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(ShowMovieInfoBox))]
-        private Media? selectedMedia;
+		[ObservableProperty]
+		[NotifyPropertyChangedFor(nameof(ShowMovieInfoBox))]
+		private Media? selectedMedia;
 
-        public bool ShowMovieInfoBox => selectedMedia is not null;
+		public bool ShowMovieInfoBox => selectedMedia is not null;
 
-        public ObservableCollection<Media> TopRated { get; set; } = new();
-        public ObservableCollection<Media> Trending { get; set; } = new();
-        public ObservableCollection<Media> NetflixOriginals { get; set; } = new();
-        public ObservableCollection<Media> ActionMovies { get; set; } = new();
+		public ObservableCollection<Media> TopRated { get; set; } = new();
+		public ObservableCollection<Media> Trending { get; set; } = new();
+		public ObservableCollection<Media> NetflixOriginals { get; set; } = new();
+		public ObservableCollection<Media> ActionMovies { get; set; } = new();
 
-        public async Task InitializeAsync()
-        {
-            var trendingListTask = _tmdbService.GetTrendingAsync();
-            var netflixOriginalsListTask = _tmdbService.GetNetflixOriginalAsync();
-            var topRatedListTask = _tmdbService.GetTopRatedAsync();
-            var actionListTask = _tmdbService.GetActionAsync();
+		public async Task InitializeAsync()
+		{
+			var trendingListTask = _tmdbService.GetTrendingAsync();
+			var netflixOriginalsListTask = _tmdbService.GetNetflixOriginalAsync();
+			var topRatedListTask = _tmdbService.GetTopRatedAsync();
+			var actionListTask = _tmdbService.GetActionAsync();
 
-            var medias = await Task.WhenAll(trendingListTask,
-                                            netflixOriginalsListTask,
-                                            topRatedListTask,
-                                            actionListTask);
+			var medias = await Task.WhenAll(trendingListTask,
+											netflixOriginalsListTask,
+											topRatedListTask,
+											actionListTask);
 
-            var trendingList = medias[0];
-            var netflixOriginalsList = medias[1];
-            var topRatedList = medias[2];
-            var actionList = medias[3];
+			var trendingList = medias[0];
+			var netflixOriginalsList = medias[1];
+			var topRatedList = medias[2];
+			var actionList = medias[3];
 
 
-            //setting random trending movie from Trending List to the Trending Movie
-            TrendingMovie = trendingList.OrderBy(t => Guid.NewGuid()).
-                                 FirstOrDefault(t =>
-                                 !string.IsNullOrWhiteSpace(t.DisplayTitle)
-                                 && !string.IsNullOrWhiteSpace(t.Thumbnail));
+			//setting random trending movie from Trending List to the Trending Movie
+			TrendingMovie = trendingList.OrderBy(t => Guid.NewGuid()).
+								 FirstOrDefault(t =>
+								 !string.IsNullOrWhiteSpace(t.DisplayTitle)
+								 && !string.IsNullOrWhiteSpace(t.Thumbnail));
 
-            SetMediaCollection(trendingList, Trending);
-            SetMediaCollection(netflixOriginalsList, NetflixOriginals);
-            SetMediaCollection(topRatedList, TopRated);
-            SetMediaCollection(actionList, ActionMovies);
-
-            //SelectedMedia == TrendingMovie;
-        }
+			SetMediaCollection(trendingList, Trending);
+			SetMediaCollection(netflixOriginalsList, NetflixOriginals);
+			SetMediaCollection(topRatedList, TopRated);
+			SetMediaCollection(actionList, ActionMovies);
+		}
 
 		private void SetMediaCollection(IEnumerable<Media> medias, ObservableCollection<Media> collection)
 		{
@@ -73,5 +74,7 @@ namespace NetflixCloneMaui.ViewModels
 				collection.Add(media);
 			}
 		}
+		[RelayCommand]
+		private void SelectMedia(Media? media = null) => selectedMedia = media;
 	}
 }
